535ae50a5acf0e3f5309386f6749ed1005542570
Jorge Mimica <jmimica@gmail.com>
2019-11-17 18:49:08 +0100

    Add db and express

--- Readme.md
+++ Readme.md
@@ -1,4 +1,12 @@
 To run mongodb, execute:
+------------------------
 mongod --dbpath /usr/local/mongodb/data
 
 DB was created in : /usr/local/mongodb/data
+
+
+To run backend, execute:
+-----------------------
+
+cd /Users/mimica/GDrive/MyCode/Mongo-Express-Angular-Node-1stApp/backend
+npm run dev
---
+++ backend/config/server.js
@@ -0,0 +1,13 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+// Use in all request to the server
+server.use(bodyParser.urlencoded( {extended: true}))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+	console.log(`Backend is running on port ${port}.`)
+})
