2e639739ed186923ae9ade030c07c5f3e4621889
Jorge Mimica <jmimica@gmail.com>
2019-11-17 19:18:17 +0100

    Add routes

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,5 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance', { useNewUrlParser: true, useUnifiedTopology: true })
+//module.exports = mongoose.connect('mongodb://localhost/db_finance')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH} é obrigatório"
